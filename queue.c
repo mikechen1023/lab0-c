6adfc4a1c7d7574c85da7d9b5dd383cfc8043624
mikechen1023 <mickycool1023@gmail.com>
2025-03-01 15:55:57 +0800

    Implement basic function of queue
    First step of lab-0
    - q_new(), q_insert_head(), q_insert_tail(), q_remove_head(), q_size()
    
    Change-Id: I03b5eea6be491a4c87d20193ef72f5325c107dae

--- queue.c
+++ queue.c
@@ -13,7 +13,16 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *li = malloc(sizeof(struct list_head));
+
+    if (!li) {
+        return NULL;
+    }
+
+    li->next = li;
+    li->prev = li;
+
+    return li;
 }
 
 /* Free all storage used by queue */
@@ -22,19 +31,70 @@ void q_free(struct list_head *head) {}
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *new_node;
+
+    new_node = (element_t *) malloc(sizeof(element_t));
+    list_add(&new_node->list, head);
+
+    new_node->value = (char *) malloc(strlen(s) + 1);
+    memcpy(new_node->value, s, strlen(s) + 1);
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *new_node;
+
+    new_node = (element_t *) malloc(sizeof(element_t));
+    list_add_tail(&new_node->list, head);
+
+    new_node->value = (char *) malloc(strlen(s) + 1);
+    memcpy(new_node->value, s, strlen(s) + 1);
+
     return true;
 }
 
 /* Remove an element from head of queue */
+/**
+ * q_remove_head() - Remove the element from head of queue
+ * @head: header of queue
+ * @sp: string would be inserted
+ * @bufsize: size of the string
+ *
+ * If sp is non-NULL and an element is removed, copy the removed string to *sp
+ * (up to a maximum of bufsize-1 characters, plus a null terminator.)
+ *
+ * NOTE: "remove" is different from "delete"
+ * The space used by the list element and the string should not be freed.
+ * The only thing "remove" need to do is unlink it.
+ *
+ * Reference:
+ * https://english.stackexchange.com/questions/52508/difference-between-delete-and-remove
+ *
+ * Return: the pointer to element, %NULL if queue is NULL or empty.
+ */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || !q_size(head))
+        return NULL;
+
+    element_t *tmp = container_of(head->next, element_t, list);
+    list_del(head->next);
+
+    if (sp) {
+        memcpy(sp, tmp->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return tmp;
 }
 
 /* Remove an element from tail of queue */
@@ -46,7 +106,17 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return -1;
+
+    int len = 0;
+    struct list_head *li;
+
+    list_for_each (li, head) {
+        len++;
+    }
+
+    return len;
 }
 
 /* Delete the middle node in queue */
